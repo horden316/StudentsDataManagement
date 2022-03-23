e3de6340bfbd2fcae1881b58f05dbc4e21456e5b
horden <horden316@gmail.com>
2022-03-23 23:52:04 +0800

    Compatible with 0.8.13 compliler
    Modify somthing to compatible with 0.8.13 compliler

--- StudentsDataManagement.sol
+++ StudentsDataManagement.sol
@@ -1,7 +1,7 @@
 /*
 author: horden.eth
 */
-pragma solidity ^0.4.22;
+pragma solidity ^0.8.13;
 contract extrawork1{
 
     struct  Student {
@@ -27,7 +27,7 @@ contract extrawork1{
 
 
     //將值塞入mapping中，塞完一次後indexcount+1
-    function inputdata (string id,address Wallet ,string Name,string Phone,string Email ) public onlyOwner{
+    function inputdata (string memory id,address Wallet ,string memory Name,string memory Phone,string memory Email ) public onlyOwner{
         StudentMap[indexcount] = Student(id,Wallet,Name ,Phone,Email);
         indexcount++;
     }
@@ -43,9 +43,9 @@ contract extrawork1{
     }
 
     //根據學號查詢index
-    function getIndexFromID(string SearchID) public view returns(uint index) {
+    function getIndexFromID(string memory SearchID) public view returns(uint index) {
         for(uint i=0;i<indexcount;i++){
-            if(keccak256(StudentMap[i].ID)==keccak256(SearchID)){
+            if(keccak256(abi.encodePacked(StudentMap[i].ID))==keccak256(abi.encodePacked(SearchID))){
                 return i;
                 break;
             }
@@ -54,27 +54,27 @@ contract extrawork1{
 
 
     //利用index查詢學生所有資料
-    function getDataFromIndex(uint index) public view returns(string ShowID,address ShowWallet ,string ShowName,string ShowPhone,string ShowEmail) {
+    function getDataFromIndex(uint index) public view returns(string memory ShowID,address ShowWallet ,string memory ShowName,string memory ShowPhone,string memory ShowEmail) {
         return (StudentMap[index].ID,StudentMap[index].walletaddress,StudentMap[index].name,StudentMap[index].phone,StudentMap[index].email);
     }
 
 
     //利ID查詢學生所有資料 (先叫getIndexFromID取得index再mapping出對應值)
-    function getDataFromID(string SearchID) public view returns(string ShowID,address ShowWallet ,string ShowName,string ShowPhone,string ShowEmail) {
+    function getDataFromID(string memory SearchID) public view returns(string memory ShowID,address ShowWallet ,string memory ShowName,string memory ShowPhone,string memory ShowEmail) {
         uint index=getIndexFromID(SearchID);
         return (StudentMap[index].ID,StudentMap[index].walletaddress,StudentMap[index].name,StudentMap[index].phone,StudentMap[index].email);
     }
 
     //根據ID修改address, phone, email (輸入格式： ID, address, phone, email)
-    function ModifyDataAccordingToID(string SearchID,address ModifyWallet,string ModifyPhone ,string ModifyEmail) public onlyOwner{
+    function ModifyDataAccordingToID(string memory SearchID,address ModifyWallet,string memory ModifyPhone ,string memory ModifyEmail) public onlyOwner{
         uint index=getIndexFromID(SearchID);
         StudentMap[index] = Student(SearchID ,ModifyWallet ,StudentMap[index].name ,ModifyPhone ,ModifyEmail);
     }
 
     //根據ID刪除該學生所以資料，就...全部設成0
-    function DeleteDataAccordingToID(string SearchID) public onlyOwner{
+    function DeleteDataAccordingToID(string memory SearchID) public onlyOwner{
         uint index=getIndexFromID(SearchID);
-        StudentMap[index] = Student('0' ,0 ,'0' ,'0' ,'0');
+        StudentMap[index] = Student('0' ,0x0000000000000000000000000000000000000000 ,'0' ,'0' ,'0');
     }
 
 
